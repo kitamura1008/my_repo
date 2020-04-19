29ee1a211edcf29743f0bd0f82df8da4c68e6ca5
Taka Kitamura <kitamura@cs.uchicago.edu>
2020-04-19 02:38:01 -0500

    transfer to my_repo

--- pa5/hash_table.py
+++ pa5/hash_table.py
@@ -1,32 +1,57 @@
 '''
 CAPP 30122 W'20: Markov models and hash tables
 
-YOUR NAME HERE
+Tetsuo Fujino
+Takayuki Kitamura
 '''
-
 
 TOO_FULL = 0.5
 GROWTH_RATIO = 2
 
 
 class HashTable:
-
+    '''
+    Class for representing a hashtable.
+    '''
     def __init__(self, cells, defval):
         '''
         Construct a new hash table with a fixed number of cells equal to the
         parameter "cells", and which yields the value defval upon a lookup to a
         key that has not previously been inserted
         '''
-        ### YOUR CODE HERE ###
-        pass
+        self.table_size = cells
+        self.defval = defval
+        self.hash_table = [defval] * self.table_size
+
+    def _hash_func(self, key):
+        '''
+        Given a set of strings, compute a integer, "hash value".
+        If the hash table is full and the imput, "key",
+        does not exist as a first element in the hash table,
+        return None.
+        '''
+        sum_ord = 0
+        for string in key:
+            sum_ord += ord(string)
+
+        for i in range(self.table_size):
+            h_value = (sum_ord + i) % self.table_size
+            if self.hash_table[h_value] == self.defval or \
+                self.hash_table[h_value][0] == key:
+                return h_value
+
+        return None
 
     def lookup(self, key):
         '''
         Retrieve the value associated with the specified key in the hash table,
         or return the default value if it has not previously been inserted.
         '''
-        ### YOUR CODE HERE ###
-        pass
+        n = self._hash_func(key)
+        if n is None or self.hash_table[n] == self.defval:
+            return self.defval
+
+        return self.hash_table[n][1]
 
     def update(self, key, val):
         '''
@@ -34,5 +59,16 @@ class HashTable:
         If "key" is not currently present in the hash table,  insert it with
         value "val".
         '''
-        ### YOUR CODE HERE ###
-        pass
+        if self.defval in self.hash_table:
+            n = self._hash_func(key)
+            self.hash_table[n] = (key, val)
+        # in cases that the table is full.
+        else:
+            # Expand the hash table.
+            copy_hash_table = self.hash_table
+            self.table_size = GROWTH_RATIO * self.table_size
+            self.hash_table = [self.defval] * self.table_size
+            for k, v in copy_hash_table:
+                self.update(k, v)
+
+            self.update(key, val)
--- pa5/markov.py
+++ pa5/markov.py
@@ -1,7 +1,8 @@
 '''
 CAPP 30122 W'20: Markov models and hash tables
 
-YOUR NAME HERE
+Tstsuo Fujino
+Takayuki Kitamura
 '''
 
 import sys
@@ -12,13 +13,20 @@ HASH_CELLS = 57
 
 
 class Markov:
-
+    '''
+    Class for representing markov model.
+    '''
     def __init__(self, k, s):
         '''
         Construct a new k-order Markov model using the statistics of string "s"
         '''
-        ### YOUR CODE HERE ###
-        pass
+        self.k_value = k
+        self.string = s
+        self.defval = None
+        self.hash_table = hash_table.HashTable(HASH_CELLS, self.defval)
+        word_dic = self.make_dic()
+        for i in word_dic:
+            self.hash_table.update(i, word_dic[i])
 
     def log_probability(self, s):
         '''
@@ -26,8 +34,60 @@ class Markov:
         character sequences modeled by this particular Markov model
         This probability is *not* normalized by the length of the string.
         '''
-        ### YOUR CODE HERE ###
-        pass
+        sum_log_pro = 0
+        num_unique_letter = len(set(self.string))
+        for i in range(len(s)):
+            prev_words = self.get_prev_words(s, i)
+            if self.hash_table.lookup(prev_words) == self.defval:
+                M = 0
+                N = 0
+            else:
+                if s[i] in self.hash_table.lookup(prev_words):
+                    M = self.hash_table.lookup(prev_words)[s[i]]
+                else:
+                    M = 0
+                N = sum(x for x in self.hash_table.lookup(prev_words).values())
+            numerator = M + 1
+            denominator = N + num_unique_letter
+            log_pro = math.log(numerator / denominator)
+            sum_log_pro += log_pro
+        return sum_log_pro
+
+    def get_prev_words(self, string, i):
+        '''
+        Get previous words, i.e., k preceding characters followed by the current character
+
+        Inputs
+            string (str): it represents total due of each property
+            i (int): the index of the letter
+
+        Output:
+            previous words(str): k preceding characters followed by the character
+        '''
+        if i >= self.k_value:
+            return string[i-self.k_value:i]
+        s = string + string[0:i]
+        return s[-self.k_value:]
+
+    def make_dic(self):
+        '''
+        Make a dictionary, whose key is previous words and value is a dictionary
+        whose key is a character and value is frequency of characters in the string.
+
+        Output:
+            word_dic(dictionary): a dictionary to manage frequency of characters
+        '''
+        word_dic = {}
+        for i in range(len(self.string)):
+            prev_words = self.get_prev_words(self.string, i)
+            if prev_words in word_dic:
+                if self.string[i] in word_dic[prev_words]:
+                    word_dic[prev_words][self.string[i]] += 1
+                else:
+                    word_dic[prev_words][self.string[i]] = 1
+            else:
+                word_dic[prev_words] = {self.string[i]:1}
+        return word_dic
 
 
 def identify_speaker(speaker_a, speaker_b, unknown_speech, k):
@@ -38,8 +98,15 @@ def identify_speaker(speaker_a, speaker_b, unknown_speech, k):
     and a conclusion of which speaker uttered the unidentified text
     based on the two probabilities.
     '''
-    ### YOUR CODE HERE ###
-    return (None, None, None)
+    a_markov = Markov(k, speaker_a)
+    b_markov = Markov(k, speaker_b)
+    log_pro_a = a_markov.log_probability(unknown_speech) / len(unknown_speech)
+    log_pro_b = b_markov.log_probability(unknown_speech) / len(unknown_speech)
+    if log_pro_a > log_pro_b:
+        most_likely_speaker = 'A'
+    else:
+        most_likely_speaker = 'B'
+    return (log_pro_a, log_pro_b, most_likely_speaker)
 
 
 def print_results(res_tuple):
